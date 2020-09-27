2a806f877cd75a25509e41197264244649633d1e
Gem <44089734+yansigit@users.noreply.github.com>
2020-09-27 23:55:41 +0900

    update

--- app/src/main/java/sb/yoon/kiosk/EnterMain.java
+++ app/src/main/java/sb/yoon/kiosk/EnterMain.java
@@ -8,6 +8,6 @@ public class EnterMain extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.enter_main);
+        setContentView(R.layout.activity_enter_main);
     }
 }
