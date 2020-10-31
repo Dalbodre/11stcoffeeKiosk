170660e002a1d1d233a9768b853c8d14f07d32d4
Gem <44089734+yansigit@users.noreply.github.com>
2020-10-31 09:29:42 +0900

    dev shopping cart

---
+++ app/src/main/java/sb/yoon/kiosk/CartFragment.java
@@ -0,0 +1,55 @@
+package sb.yoon.kiosk;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import sb.yoon.kiosk.controller.CartListAdapter;
+import sb.yoon.kiosk.model.CartMenu;
+
+
+public class CartFragment extends ListFragment {
+
+    private List<CartMenu> cartMenuList;
+    private CartListAdapter adapter;
+
+    public CartFragment(List<CartMenu> menuList) {
+        this.cartMenuList = menuList;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 인덱스 표시 어댑터 설정
+        adapter = new CartListAdapter(cartMenuList);
+        // 어댑터를 설정
+        setListAdapter(adapter);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //View view = super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_cart, container, false);
+        return view;
+    }
+
+    public void delCartMenuList(int position) {
+        this.cartMenuList.remove(position);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void addCartMenuList(CartMenu cartMenu) {
+        this.cartMenuList.add(cartMenu);
+        adapter.notifyDataSetChanged();
+    }
+}
---
+++ app/src/main/java/sb/yoon/kiosk/controller/CartListAdapter.java
@@ -0,0 +1,66 @@
+package sb.yoon.kiosk.controller;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+
+import java.util.List;
+
+import sb.yoon.kiosk.R;
+import sb.yoon.kiosk.layout.ItemElement;
+import sb.yoon.kiosk.model.CartMenu;
+import sb.yoon.kiosk.model.Ingredient;
+import sb.yoon.kiosk.model.Menu;
+
+// 어뎁터 클래스
+public class CartListAdapter extends BaseAdapter {
+    private List<CartMenu> cartMenuList;
+
+    public CartListAdapter(List<CartMenu> menuList){
+        this.cartMenuList = menuList;
+    }
+
+    @Override
+    public int getCount() {
+        return cartMenuList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return cartMenuList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        final Context context = parent.getContext();
+        // 특정 행의 데이터 구함
+        CartMenu cartMenu = (CartMenu)getItem(position);
+
+        // View는 재사용되기 때문에 처음에만 리스트 아이템 표시용 레이아웃을 읽어와서 생성함
+        if(convertView == null){
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        // View의 각 Widget에 데이터 저장
+        ItemElement view = convertView.findViewById(R.id.menu_element);
+        Drawable drawable = cartMenu.getIcon();
+        view.setImageDrawable(drawable);
+        view.setText(cartMenu.getName());
+
+        return convertView;
+    }
+}
