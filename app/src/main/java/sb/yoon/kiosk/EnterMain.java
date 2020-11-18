4c1e421918542d13025cfdbfe87f4cb14616ca7c
Yoon Seong Bin <44089734+yansigit@users.noreply.github.com>
2020-11-18 12:39:15 +0900

    update kiosk main and order number popup

--- app/src/main/java/sb/yoon/kiosk/EnterMain.java
+++ app/src/main/java/sb/yoon/kiosk/EnterMain.java
@@ -5,16 +5,27 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class EnterMain extends AppCompatActivity {
-
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_main);
+
+        progressBar = findViewById(R.id.progressBar);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(ProgressBar.GONE);
     }
 
     public void buttonClicked(View view) {
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(ProgressBar.VISIBLE);
         Intent intent = new Intent(this, KioskListActivity.class);
         startActivity(intent);
 
