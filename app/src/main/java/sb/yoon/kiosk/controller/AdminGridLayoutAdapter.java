fd3c95b3034bb84a53ecf9512f70b30b09e88797
Yoon Seong Bin <44089734+yansigit@users.noreply.github.com>
2021-01-29 14:52:28 +0900

    수정, 삭제 버튼

--- app/src/main/java/sb/yoon/kiosk/controller/AdminGridLayoutAdapter.java
+++ app/src/main/java/sb/yoon/kiosk/controller/AdminGridLayoutAdapter.java
@@ -11,6 +11,7 @@ import sb.yoon.kiosk.model.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,7 +30,6 @@ import sb.yoon.kiosk.model.Option;
 public class AdminGridLayoutAdapter extends BaseAdapter {
     //메뉴 리스트
     private List<Menu> menuList;
-
     private Context context;
 
     public AdminGridLayoutAdapter(List<Menu> menuList, Context context){
@@ -103,6 +103,14 @@ public class AdminGridLayoutAdapter extends BaseAdapter {
         menuItem.setTag(pos);
         menuItem.setOnClickListener(new MenuOnClickListener());
 
+        Button modify_button = view.findViewById(R.id.admin_modify_button);
+        modify_button.setTag(pos);
+        modify_button.setOnClickListener(new ButtonOnClickListener());
+
+        Button delete_button = view.findViewById(R.id.admin_delete_button);
+        delete_button.setTag(pos);
+        delete_button.setOnClickListener(new ButtonOnClickListener());
+
         return view;
     }
 
@@ -122,6 +130,22 @@ public class AdminGridLayoutAdapter extends BaseAdapter {
                     "drawable", context.getPackageName()));
             List<Option> options = menu.getOptionList();
             Long menuId = menu.getId();
+        }
+    }
+
+    // 수정, 삭제 버튼
+    class ButtonOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            int position = (int) v.getTag();
+
+            if (v.getId() == R.id.admin_modify_button) {
+                Toast.makeText(context.getApplicationContext(), "수정버튼", Toast.LENGTH_SHORT).show();
+            } else if (v.getId() == R.id.admin_delete_button) {
+                menuList.get(position).delete();
+                menuList.remove(position);
+                AdminGridLayoutAdapter.this.notifyDataSetChanged();
+            }
         }
     }
 }
